dd233e519ec6b4b9d478227aa79af5c575dd9fcc
Alec Sloan <sloan.alec@gmail.com>
2021-10-14 22:39:21 -0700

    Update scrape script to be a bit more efficient by only scraping vox for sale.

--- scrape.js
+++ scrape.js
@@ -1,102 +1,127 @@
 const playwright = require('playwright');
 var fs = require('fs');
-var schedule = require('node-schedule');
 
-const intervalMin = 240
-const loadTime = 2500
-const buyThreshold = 0.01
-const voxStartingPoint = 0
+//TODO: drop these in favor of waiting for components to load
+const catalogLoadTime = 6000
+const detailLoadTime = 5000
 
-async function main() {
-    const jobs = {};
+async function scrapeVox() {
+    const browser = await playwright.chromium.launch({ headless: false });
 
-    console.log('\n Searching VOX ')
+    //Get a list of for sale vox
+    console.log("### Gathering a list of VOX for sale ###")
 
-    console.log('First job on ' + new Date().toLocaleString("en-US") + '\n')
+    const page = await browser.newPage();
 
-    jobs['rarityCheckerJob'] = schedule.scheduleJob(Date.now() + 1000, async () => {
-        await runJob().catch(e => console.log('Error -> scheduling a new Job' + e)).then(() => {
-            var nextSchedule = new Date(new Date().getTime() + 60 * intervalMin * 1000);
-            console.log('Reschedule rarityCheckerJob at ' + nextSchedule.toLocaleString("en-US"));
-            jobs['rarityCheckerJob'].reschedule(nextSchedule);
-        })
+    await page.goto('https://rarity.tools/collectvox?filters=%24buyNow%24On%3Atrue');
 
-    });
+    let buyableVOX = []
 
-}
+    while (true) {
+      await new Promise(function (resolve) {
+        setTimeout(resolve, catalogLoadTime)
+      });
 
-async function runJob() {
-    const browser = await playwright.chromium.launch({ headless: true });
+      const buyableVOXURLs = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('div.overflow-hidden.rounded-md > a')).map(item => item.href);
+      });
+
+      buyableVOX = buyableVOX.concat(buyableVOXURLs)
+
+      console.log(buyableVOX.length + " in list");
+
+      const nextPage = await page.locator('text=Next >').first();
+
+      const foundVOX = await page.locator('text=Matching').first().innerText()
+
+      if (buyableVOX.length == 48)
+        break
+
+      if (buyableVOX.length >= foundVOX.match(/\d/g).join("")) {
+        break
+      }
+
+      nextPage.click();
+    }
+
+    page.close();
+
+    //Visit each VOX's detail page to gather info
+
+    console.log("### Visiting each VOX's detail page to gather specific info ###")
 
     const date = new Date().toLocaleDateString('en-US').replace(/\//g, '-')
 
-    const buyVoxFileName = __dirname + '/data/buyVox-' + date + '.json'
+    const voxFileName = __dirname + '/src/data/VOX-' + date + '.json'
 
-    for (let voxNumber = 0 + voxStartingPoint; voxNumber < 8887 - voxStartingPoint; voxNumber++) {
-      const page = await browser.newPage();
+    for (let i = 0; i < buyableVOX.length; i++) {
+      const page = await browser.newPage()
 
-      await page.goto('https://rarity.tools/collectvox/view/' + voxNumber);
+      const voxURL = buyableVOX[i]
+
+      await page.goto(voxURL)
 
       await new Promise(function (resolve) {
-          setTimeout(resolve, loadTime)
+          setTimeout(resolve, detailLoadTime)
       });
 
       const price = await page.$eval('div.h-full.font-bold.text-green-500', div => {
           return div.innerText.replace(' ETH','')
-      }).catch(() => console.log("Failed to get price for VOX " + voxNumber));
+      }).catch(() => console.log("Failed to get price for VOX " + voxURL))
+      const id = await page.$eval('div.flex-grow.text-sm.text-right.text-gray-400', div => {
+        return div.innerText
+      }).catch(() => console.log("Failed to get name for VOX " + voxURL))
+      const image = await page.$eval('img.block.p-0', img => {
+        return img.src
+      }).catch(() => console.log("Failed to get name for VOX " + voxURL))
+      const name = await page.$eval('div.text-lg.font-bold.text-left.text-pink-700', div => {
+          return div.innerText
+      }).catch(() => console.log("Failed to get name for VOX " + voxURL))
+      const opensea = await page.$eval('a.bgInput', a => {
+          return a.href
+      }).catch(() => console.log("Failed to get opensea link for VOX " + voxURL))
+      const rank = await page.$eval('span.font-bold.whitespace-nowrap', span => {
+          return span.innerText.replace('Rarity Rank #', '')
+      }).catch(() => console.log("Failed to get rank for VOX " + voxURL))
+      const rarity = await page.$eval('div.px-2.mx-1.mb-0.text-lg.font-extrabold.text-green-500.bg-white.rounded-md', div => {
+          return div.innerText
+      }).catch(() => console.log("Failed to get rarity for VOX " + voxURL))
 
-      if (price != '') {
-        const name = await page.$eval('div.text-lg.font-bold.text-left.text-pink-700', div => {
-            return div.innerText
-        }).catch(() => console.log("Failed to get name for VOX " + voxNumber));
-        const opensea = await page.$eval('a.bgInput', a => {
-            return a.href
-        }).catch(() => console.log("Failed to get opensea link for VOX " + voxNumber));
-        const rank = await page.$eval('span.font-bold.whitespace-nowrap', span => {
-            return span.innerText.replace('Rarity Rank #', '')
-        }).catch(() => console.log("Failed to get rank for VOX " + voxNumber));
-        const rarity = await page.$eval('div.px-2.mx-1.mb-0.text-lg.font-extrabold.text-green-500.bg-white.rounded-md', div => {
-            return div.innerText
-        }).catch(() => console.log("Failed to get rarity for VOX " + voxNumber));
+      page.close()
 
-        page.close()
-
-        let vox = {
-          price: price,
-          name:  name,
-          opensea: opensea,
-          rank: rank,
-          rarity: rarity,
-          ethPerTC: price / rarity
-        }
-
-        if ((price / rarity) <= buyThreshold) {
-          fs.readFile(buyVoxFileName, function (err, data) {
-            if (!data) {
-              data = '[]'
-            }
-
-            var json = JSON.parse(data)
-            json.push(vox)
-
-            fs.writeFile(buyVoxFileName, JSON.stringify(json, undefined, 4), (error) => {
-              if (error) {
-                console.log(error)
-              }
-              else {
-                console.log("Buy VOX saved.")
-              }
-            })
-          })
-        }
-
-        console.log(vox)
-      } else {
-        console.log("VOX " + voxNumber + " not for sale.")
+      let vox = {
+        id: id,
+        image: image,
+        price: price,
+        name:  name,
+        opensea: opensea,
+        rank: rank,
+        rarity: rarity,
+        ethPerRarity: price / rarity
       }
+
+      console.log(vox)
+
+      fs.readFile(voxFileName, function (err, data) {
+        if (!data) {
+          data = '[]'
+        }
+
+        var json = JSON.parse(data)
+        json.push(vox)
+
+        fs.writeFile(voxFileName, JSON.stringify(json, undefined, 4), (error) => {
+          if (error) {
+            console.log(error)
+          }
+          else {
+            console.log("VOX " + (i + 1) + " of " + buyableVOX.length + " saved.")
+          }
+        })
+      })
     }
 
     browser.close()
-};
+}
 
-main()
+scrapeVox()
