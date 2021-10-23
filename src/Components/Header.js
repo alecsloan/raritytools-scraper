96ff67562e2a38956140ff65f3652610cccd2e85
Alec Sloan <sloan.alec@gmail.com>
2021-10-22 17:55:40 -0700

    Add disclaimer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import {AppBar, IconButton, Toolbar} from "@mui/material";
-import {Brightness2, Brightness7} from "@mui/icons-material";
+import React, {useState} from 'react'
+import {Alert, AlertTitle, AppBar, Button, IconButton, Slide, Snackbar, Toolbar} from "@mui/material";
+import {Brightness2, Brightness7, ThumbUp} from "@mui/icons-material";
 
 import * as Theme from "../Theme/index"
 
 function Header (props) {
+  const [theyUnderstand, setTheyUnderstand] = useState(localStorage.getItem("theyUnderstand"));
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -18,6 +20,30 @@ function Header (props) {
           {props.theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness2 /> }
         </IconButton>
       </Toolbar>
+
+      <Snackbar
+        key={"understand"}
+        open={!theyUnderstand}
+      >
+        <Alert
+          action={
+            <IconButton
+              aria-label="got-it"
+              color="inherit"
+              onClick={() => {
+                localStorage.setItem("theyUnderstand", true)
+                setTheyUnderstand(true)
+              }}
+              size="small"
+            >
+              <ThumbUp />
+            </IconButton>
+          }
+          severity='info'
+        >
+          This is a community project and not affiliated with Gala Games.
+        </Alert>
+      </Snackbar>
     </AppBar>
   );
 }
