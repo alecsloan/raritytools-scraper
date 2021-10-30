4caa8054aacb41ec4d0f3cf07aef2c4570edbddb
Alec Sloan <sloan.alec@gmail.com>
2021-10-29 22:51:42 -0700

    Add a 10 minute delay timer on fetching new data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {CssBaseline, Grid, ThemeProvider} from "@mui/material";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 import Header from "./Components/Header";
 import NFTTable from "./Components/NFTTable";
@@ -10,63 +10,69 @@ import backupNFTs from "./data/VOX.json";
 import voxRarity from "./data/VOX-rarity.json";
 
 function App() {
+  const minuteCooldown = 10;
+
   const [theme, setTheme] = useState(Theme.dark)
+  const [nfts, setNFTs] = useState(JSON.parse(localStorage.getItem("nfts")) || backupNFTs)
 
-  const[nfts, setNFTs] = useState(backupNFTs)
+  const getNFTs = useCallback(async () => {
+    let openseaAPIURLs = []
 
-  useEffect(() => {
-    const getNFTs = async () => {
-      let openseaAPIURLs = []
-
-      for (let offset = 0; offset < 8888; offset += 50) {
-        openseaAPIURLs.push(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=50&collection=collectvox`)
-      }
-
-      let assets = []
-
-      await Promise.all(openseaAPIURLs.map(url => fetch(url)))
-        .then(responses =>
-          Promise.all(responses.map(res => res.json()))
-        ).then(json => {
-          return json.forEach(subData => assets = assets.concat(subData.assets.filter(nft => (nft.sell_orders && nft.sell_orders[0].payment_token === "0x0000000000000000000000000000000000000000"))))
-        }).catch(() =>  {
-          console.log("Failed to connect to Opensea")
-
-          return
-        })
-
-      if (assets.length === 0)
-        return
-
-      assets = assets.map((asset) => {
-        const id = asset.token_id
-
-        const vox = voxRarity.find(v => v.id === id)
-        const price = asset.sell_orders[0].base_price /= Math.pow(10, 18)
-
-        return {
-          "id": id,
-          "image": asset.image_thumbnail_url,
-          "price": price,
-          "name": asset.name,
-          "opensea": asset.permalink,
-          "rank": vox.rank,
-          "rarity": vox.rarity,
-          "ethPerRarity": price / vox.rarity
-        }
-      })
-
-      setNFTs(assets)
+    for (let offset = 0; offset < 8888; offset += 50) {
+      openseaAPIURLs.push(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=${offset}&limit=50&collection=collectvox`)
     }
 
-    getNFTs()
-  }, [setNFTs])
+    let assets = []
+
+    await Promise.all(openseaAPIURLs.map(url => fetch(url)))
+      .then(responses =>
+        Promise.all(responses.map(res => res.json()))
+      ).then(json => {
+        return json.forEach(subData => assets = assets.concat(subData.assets.filter(nft => (nft.sell_orders && nft.sell_orders[0].payment_token === "0x0000000000000000000000000000000000000000"))))
+      }).catch(() =>  {
+        console.log("Failed to connect to Opensea")
+      })
+
+    if (assets.length === 0)
+      return
+
+    assets = assets.map((asset) => {
+      const id = asset.token_id
+
+      const vox = voxRarity.find(v => v.id === id)
+      const price = asset.sell_orders[0].base_price /= Math.pow(10, 18)
+
+      return {
+        "id": id,
+        "image": asset.image_thumbnail_url,
+        "price": price,
+        "name": asset.name,
+        "opensea": asset.permalink,
+        "rank": vox.rank,
+        "rarity": vox.rarity,
+        "ethPerRarity": price / vox.rarity
+      }
+    })
+
+    setNFTs(assets)
+
+    localStorage.setItem("dataUpdated", new Date().getTime())
+    localStorage.setItem("nfts", JSON.stringify(nfts))
+  }, [nfts])
+
+  useEffect(() => {
+    const dataUpdated = localStorage.getItem("dataUpdated")
+
+    if (!dataUpdated || ((new Date().getTime() - dataUpdated) / (minuteCooldown * 60000) > minuteCooldown)) {
+      getNFTs()
+    }
+  }, [setNFTs, getNFTs])
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <Header setTheme={setTheme.bind(this)} theme={theme} />
+      <Header getNFTs={getNFTs.bind(this)} minuteCooldown={minuteCooldown} setTheme={setTheme.bind(this)} theme={theme} />
 
       <Statistics nfts={nfts} />
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,41 @@
 import React, {useEffect, useState} from 'react'
-import {Alert, AppBar, Grid, IconButton, Snackbar, Toolbar} from "@mui/material";
-import {Brightness2, Brightness7, LocalGasStation, ThumbUp} from "@mui/icons-material";
+import {Alert, AppBar, Box, CircularProgress, Grid, IconButton, Snackbar, Toolbar, Typography} from "@mui/material";
+import {Brightness2, Brightness7, LocalGasStation, Refresh, ThumbUp} from "@mui/icons-material";
 
 import * as Theme from "../Theme/index"
 import {LoadingButton} from "@mui/lab";
 
+function CircularProgressWithLabel(props) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant="caption" component="div" color="text.secondary">
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
 function Header (props) {
-  const [theyUnderstand, setTheyUnderstand] = useState(localStorage.getItem("theyUnderstand"));
-  const [gas, setGas] = useState(false);
+  const [theyUnderstand, setTheyUnderstand] = useState(localStorage.getItem("theyUnderstand"))
+  const [gas, setGas] = useState(false)
+
+  const dataUpdated = localStorage.getItem("dataUpdated")
+
+  const cooldownPercent = ((new Date().getTime() - dataUpdated) / (props.minuteCooldown * 60000) * 100)
 
   useEffect(
     () => {
@@ -18,7 +46,7 @@ function Header (props) {
       });
 
       return () => {
-        socket.close();
+        socket.close()
       }
     }
   )
@@ -37,9 +65,27 @@ function Header (props) {
             >
               {props.theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness2 /> }
             </IconButton>
+            <IconButton
+              aria-label='mode'
+              className='p-1 pull-left'
+              color='inherit'
+              disabled={dataUpdated && (cooldownPercent < 100)}
+              onClick={() => {
+                if (cooldownPercent >= 100) {
+                  props.getNFTs()
+                }
+              }}
+              title='Refresh Data'
+            >
+              {
+                (dataUpdated && (cooldownPercent < 100))
+                  ? <CircularProgressWithLabel value={cooldownPercent} />
+                  : <Refresh />
+              }
+            </IconButton>
           </Grid>
 
-          <Grid item xs={6}></Grid>
+          <Grid item xs={6} />
 
           <Grid item xs={3}>
             <LoadingButton
