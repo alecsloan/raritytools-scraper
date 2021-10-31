8c36be5e948741f7b332fbc3b0bb7fb2f54159db
Alec Sloan <sloan.alec@gmail.com>
2021-10-31 14:58:35 -0700

    Add initial implementation of zoom

--- src/Components/NFTValueCurve.js
+++ src/Components/NFTValueCurve.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import {Scatter} from "react-chartjs-2";
+import { Chart } from 'chart.js';
+import zoomPlugin from 'chartjs-plugin-zoom';
+import {Button, IconButton, Tooltip, Typography} from "@mui/material";
+import {Info} from "@mui/icons-material";
+
+Chart.register(zoomPlugin);
 
 function NFTValueCurve (props) {
   const uniqueNFTs =
@@ -8,6 +14,8 @@ function NFTValueCurve (props) {
       .map(id => {
         return props.nfts.find(nft => nft.id === id)
       })
+
+  let nftChart = null;
 
   const data = {
     datasets: [
@@ -20,6 +28,32 @@ function NFTValueCurve (props) {
   };
 
   const options = {
+    plugins: {
+      zoom: {
+        pan: {
+          enabled: true,
+          modifierKey: 'shift'
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+            modifierKey: 'shift'
+          },
+          drag: {
+            enabled: true,
+            modifierKey: 'ctrl'
+          },
+          onZoom: function({chart}) { nftChart = chart }
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function(ctx) {
+            return [`Name: ${ctx.raw.label}`, `Rarity: ${ctx.raw.y}`, ` Price: ${ctx.raw.x}`]
+          }
+        }
+      }
+    },
     scales: {
       x: {
         ticks: {
@@ -48,25 +82,38 @@ function NFTValueCurve (props) {
         },
       }
     },
-    plugins: {
-      tooltip: {
-        callbacks: {
-          label: function(ctx) {
-            return [`Name: ${ctx.raw.label}`, `Rarity: ${ctx.raw.y}`, ` Price: ${ctx.raw.x}`]
-          }
-        }
-      }
-    },
     tooltips: {
       enabled: false
     }
   };
 
   return (
-    <Scatter
-      data={data}
-      options={options}
-    />
+    <div>
+      <Scatter
+        data={data}
+        options={options}
+      />
+      <Button
+        disabled={nftChart !== null}
+        onClick={() => nftChart ? nftChart.resetZoom() : null}>
+        Reset Zoom
+      </Button>
+      <Tooltip
+        placement="top"
+        title={
+          <React.Fragment>
+            <Typography color="inherit" fontSize={20}>About Zooming</Typography>
+            <h3>Shift + mouse wheel to zoom.</h3>
+            <h3>Shift + drag the mouse to pan.</h3>
+            <h3>CTRL + drag the mouse to select.</h3>
+          </React.Fragment>
+        }
+      >
+        <IconButton>
+          <Info />
+        </IconButton>
+      </Tooltip>
+    </div>
   );
 }
 
